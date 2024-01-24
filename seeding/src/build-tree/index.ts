d3ab888bfee4ea0857bd1312e70df55abd5a5ce5
Andrew <globalshow6@gmail.com>
2024-01-24 18:33:15 +0300

    Change Tree on Russian Tree

--- seeding/src/build-tree/index.ts
+++ seeding/src/build-tree/index.ts
@@ -3,18 +3,8 @@ import { SkillTree } from "./types";
 import fetch from "cross-fetch";
 
 const PASSIVE_TREE_JSON = {
-  "3_18":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/85c09648bbb9447474f84f48a942738ce3e5bdb4/data.json",
-  "3_19":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/479be10a08d51c0a798cd693701a6bd6300738dc/data.json",
-  "3_20":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/d2a0ffa5e15717e0115277f8bd852c6b53371429/data.json",
-  "3_21":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/8c5a952499d7e58e29261f9ae1fd9c4a8b55f6b1/data.json",
-  "3_22":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/ec48bb1f5f25cfdc26093904bbf1b2529d18387e/data.json",
   "3_23":
-    "https://raw.githubusercontent.com/grindinggear/skilltree-export/d1e926cc3b3568750311e7d2e791b2a34efad7c2/data.json",
+    "https://raw.githubusercontent.com/ShestakovAndrew/exile-leveling-russian/main/seeding/src/build-tree/trees/tree.json",
 };
 
 export async function buildTemplates() {
